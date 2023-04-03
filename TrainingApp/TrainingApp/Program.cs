506a0b517df93947be4365dbf5c368eca3b5d9a0
magda <magda@LAPTOP-JBFKPILU>
2023-04-03 03:37:19 +0200

    organizacja kodu, będę dodawać funkcje

--- TrainingApp/TrainingApp/Program.cs
+++ TrainingApp/TrainingApp/Program.cs
@@ -13,19 +13,71 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
-    
-    static uint silnia=1;
-
+    static Stopwatch StopWatch = new System.Diagnostics.Stopwatch();
     static void Main(string[] args)
     {
-        System.Diagnostics.Stopwatch StopWatch = new System.Diagnostics.Stopwatch();
+        Console.WriteLine("Wybierz:");
+        Console.WriteLine("1    - praca na długiej liczbie");
+        Console.WriteLine("2    - praca na krótkiej liczbie");
+        Console.WriteLine("3    - praca na pliku");
+        Console.WriteLine("4    - praca na klasie");
+        Console.WriteLine("5    - długości typów");
+        Console.WriteLine("q    - quit");
 
-    Console.WriteLine("Wybierz:");
-        Console.WriteLine("(1) Silnia iteracyjnie");
-        Console.WriteLine("(2) Silnia rekurencyjnie");
-        Console.WriteLine("(3) Liczby pierwsze lista");
-        Console.WriteLine("(4) Znaki w kodzie ASCI");
-        Console.WriteLine("(5) Sortowanie bąbelkowe");
+        var option = Console.ReadLine();
+        while (option != "q")
+        {
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Praca na długiej liczbie (sortowanie, liczby pierwsze)");
+                    WorkOnNumber();
+                    break;
+                case "2":
+                    Console.WriteLine("Praca na krótkiej liczbie (parsowanie PESEL, NIP, ASCI)");
+                    break;
+                case "3":
+                    Console.WriteLine("Praca na pliku (zapis w różnym trybie i odczyt)");
+                    break;
+                case "4":
+                    Console.WriteLine("Praca na klasie (dodaj do listy, usuń, eventy, wyjątki)");
+                    break;
+                case "5":
+                    Console.WriteLine("Długości typów prostych");
+                    TypeLengths();
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiej opcji, wybierz 1-5 lub q");
+                    break;
+            }
+            option = Console.ReadLine();
+        }
+        Console.Write("Do zobaczenia");
+        Console.ReadKey();
+    }
+
+    static void TypeLengths()
+    {
+        Console.WriteLine("ulong: \t" + ulong.MaxValue);
+        Console.WriteLine("long: \t" + long.MaxValue);
+        Console.WriteLine("uint: \t" + uint.MaxValue);
+        Console.WriteLine("int: \t" + int.MaxValue);
+        Console.WriteLine("float: \t" + float.MaxValue);
+        Console.WriteLine("double: \t" + double.MaxValue);
+        Console.WriteLine("decimal: \t" + decimal.MaxValue);
+        Console.WriteLine("char: \t" + char.MaxValue);
+    }
+
+
+    static void WorkOnNumber()
+    {
+        Console.WriteLine("Wybierz:");
+        Console.WriteLine("(1a) Silnia iteracyjnie");
+        Console.WriteLine("(1b) Silnia rekurencyjnie");
+        Console.WriteLine("(2) Liczby pierwsze lista");
+        Console.WriteLine("(3) Znaki w kodzie ASCI");
+        Console.WriteLine("(4a) Sortowanie bąbelkowe");
+        Console.WriteLine("(4b) Sortowanie szybkie");
 
         var option = Console.ReadLine();
         Console.WriteLine("Wybrano: " + option);
@@ -36,49 +88,67 @@ class Program
             Console.WriteLine(number + "\nFormat nieprawidłowy, podaj liczbę całkowitą dodatnią");
             number = Console.ReadLine();
         }
-
+        
         var numberValid = uint.Parse(number);
         StopWatch.Start();
         switch (option)
         {
-            case "1" :
+            case "1a":
                 Console.WriteLine("Silnia iteracyjnie");
                 uint silniaIterative = IterativeSilnia(numberValid);
                 WriteLineInColor(ConsoleColor.Green, silniaIterative.ToString() + "\n");
                 break;
-            case "2":
+            case "1b":
                 Console.WriteLine("Silnia rekurencyjnie");
                 uint silniaRecursive = RecursiveSilnia(numberValid);
                 WriteLineInColor(ConsoleColor.Green, silniaRecursive.ToString() + "\n");
                 break;
-            case "3":
+            case "2":
                 Console.WriteLine("Liczby pierwsze");
                 var primes = FindPrimes(numberValid);
                 var foundPrimes = PrintList<uint>("Primes", primes);
                 WriteLineInColor(ConsoleColor.Green, foundPrimes);
                 break;
-            case "4":
+            case "3":
                 Console.WriteLine("Znaki w kodzie ASCI");
                 ASCI(numberValid);
                 break;
-            case "5":
+            case "4a":
                 Console.WriteLine("Sortowanie bąbelkowe");
                 var randomList = RandomList((int)numberValid);
-                var toPrintSort = PrintList<int>("Unsorted", randomList);
-                WriteLineInColor(ConsoleColor.Green, toPrintSort);
+                var randomNumbers = PrintList<int>("Unsorted", randomList);
+                WriteLineInColor(ConsoleColor.Green, randomNumbers);
                 var sortedList = BubbleSort(randomList);
-                var toPrintUnsort = PrintList<int>("Sorted", sortedList);
-                WriteLineInColor(ConsoleColor.Green, toPrintUnsort);
+                var sortedNumbers = PrintList<int>("Sorted", sortedList);
+                WriteLineInColor(ConsoleColor.Blue, sortedNumbers);
+                break;
+            case "4b":
+                Console.WriteLine("Sortowanie szybkie");
                 break;
             default:
                 Console.WriteLine("Nie ma takiej opcji");
                 break;
         }
-
         StopWatch.Stop();
         Console.WriteLine("Time elapsed: " + StopWatch.Elapsed);
-        Console.Write("Press anything to exit...");
-        Console.ReadKey();
+        StopWatch.Reset();
+    }
+
+    static void WorkOnShortNumber()
+    {//PESEL, NIP, ASCI
+        string shortNumber="";
+        Console.WriteLine(shortNumber);
+    }
+
+    static void WorkOnFile(string file, string testToWrite)
+    {
+        Console.WriteLine(file);
+        Console.WriteLine(testToWrite);
+    }
+
+    static void WorkOnDateTime(DateTime date)
+    {
+        Console.WriteLine(date.ToString());
     }
 
     static string PrintList<T>(string title, List<T> list)
